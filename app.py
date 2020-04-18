24cc38196f5066da8dbd82969a016bc10f41e12a
Emily Aquin <eaquin1@gmail.com>
2020-04-18 18:42:42 -0500

    seeded database blogly

---
+++ app.py
@@ -0,0 +1,20 @@
+"""Blogly application."""
+
+from flask import Flask
+from models import db, connect_db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+# db.create_all()
+
+from flask_debugtoolbar import DebugToolbarExtension
+app.config['SECRET_KEY'] = "SECRET!"
+debug = DebugToolbarExtension(app)
+
+@app.route("/")
+def render_userlist():
+    """List users"""
---
+++ seed.py
@@ -0,0 +1,28 @@
+from models import User, db
+from app import app
+
+#Create all tables
+db.drop_all()
+db.create_all()
+
+# If the table isn't empty, empty it
+User.query.delete()
+
+#Add users
+alda = User(first_name="Alan", last_name="Alda", image_url="https://images.unsplash.com/photo-1587224702711-315d3c7f296d")
+burton = User(first_name="Joel", last_name="Burton", image_url="https://images.unsplash.com/photo-1577904624593-bf2e2692efce")
+smith = User(first_name="Jane", last_name="Smith", image_url="https://images.unsplash.com/photo-1585393311457-f99f89ae7004")
+swift = User(first_name="Gail", last_name="Swift", image_url="https://images.unsplash.com/photo-1585393313732-03777bb1f461")
+cures = User(first_name="Jimmy", last_name="Cures", image_url="https://images.unsplash.com/photo-1587160046330-8efc5abe41fa")
+wexler = User(first_name="Kim", last_name="Wexler", image_url="https://images.unsplash.com/photo-1583183712060-99c0ae2c42d")
+
+# Add new objects to session, so they'll persist
+db.session.add(alda)
+db.session.add(burton)
+db.session.add(smith)
+db.session.add(swift)
+db.session.add(cures)
+db.session.add(wexler)
+
+# Commit--otherwise, this never gets saved!
+db.session.commit()
